f6e422284117303c8af66a0a2b2496ebe2237383
Jakub Šťastný aka Botanicus <knava.bestvinensis@gmail.com>
2008-12-26 20:47:49 +0100

    Logging if  or , load Microgems just if RubyGems aren't loaded

--- microgems.rb
+++ microgems.rb
@@ -1,87 +1,110 @@
 require 'rbconfig'
 
-class Microgem
-  VERSION = "1.0.0"
-  class << self
-    # TODO: configuration
-    # TODO: modularize, ~/.gems
-    def root
-      File.join(self.path, "gems")
+unless $LOADED_FEATURES.find { |file| file.match(/rubygems/) }
+  module Gem
+    # Gem::LoadError: Could not find RubyGem sdfsdf (>= 0)
+    class LoadError < ::LoadError
     end
+  end
 
-    # Microgem.gems("merb-core")
-    # => ["/usr/lib/ruby/gems/1.8/gems/merb-core-0.9.8/lib"]
-    def gems(name)
-      Dir["#{self.root}/#{name}-*/lib"]
-    end
-
-    # Microgem.gem("merb-core
-    def gem(name, version)
-      if version 
-        File.join(self.root, "#{name}-#{version}", "lib")
-      else
-        self.gems.last
+  class Microgems
+    VERSION = "1.0.0"
+    class << self
+      # TODO: configuration
+      # TODO: modularize, ~/.gems
+      def root
+        File.join(self.path, "gems")
       end
-    end
 
-    def loaded_gems
-      @loaded_gems ||= {}
-    end
-
-    # stolen from minigems
-    def path
-      @default_path ||= if defined? RUBY_FRAMEWORK_VERSION then
-        File.join File.dirname(RbConfig::CONFIG["sitedir"]), 'Gems', 
-          RbConfig::CONFIG["ruby_version"]
-      elsif defined?(RUBY_ENGINE) && File.directory?(
-        File.join(RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
-          RbConfig::CONFIG["ruby_version"])
-        )
-          File.join RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
-            RbConfig::CONFIG["ruby_version"]
-      else
-        File.join RbConfig::CONFIG["libdir"], 'ruby', 'gems', 
-          RbConfig::CONFIG["ruby_version"]
+      # Microgem.gems("merb-core")
+      # => ["/usr/lib/ruby/gems/1.8/gems/merb-core-0.9.8/lib"]
+      def gems(name)
+        Dir["#{self.root}/#{name}-*/lib"]
       end
-    end
 
-    def activate(name, version = nil)
-      if version.nil?
-        $:.push(self.gem(self.gems(name).last))
-      else
-        # TODO: > = ~ version
-        version.delete("<>=~")
-        $:.push(self.gem(name, version))
-      end
-    end
-
-    def deactivate(name, version = nil)
-      if version.nil?
-        $:.each do |path|
-          if File.dirname(path).match(/^#{Regexp::quote(name)}-/)
-            $:.delete(path)
-          end
+      # Microgem.gem("merb-core", "0.9.8")
+      # => "/usr/lib/ruby/gems/1.8/gems/merb-core-0.9.8/lib"
+      def gem(name, version)
+        if version.nil?
+          self.gems(name).last
+        else
+          dir = File.join(self.root, "#{name}-#{version}", "lib")
+          return self.gem(name, nil) unless File.directory?(dir)
         end
-      else
-        # TODO: > = ~ version
-        version.delete("<>=~")
-        libdir = self.gem(name, version)
-        $:.delete(libdir) if $:.include?(libdir)
+      end
+
+      def loaded_gems
+        @loaded_gems ||= Array.new
+      end
+
+      # stolen from minigems
+      def path
+        @default_path ||= if defined? RUBY_FRAMEWORK_VERSION then
+          File.join File.dirname(RbConfig::CONFIG["sitedir"]), 'Gems', 
+            RbConfig::CONFIG["ruby_version"]
+        elsif defined?(RUBY_ENGINE) && File.directory?(
+          File.join(RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
+            RbConfig::CONFIG["ruby_version"])
+          )
+            File.join RbConfig::CONFIG["libdir"], RUBY_ENGINE, 'gems', 
+              RbConfig::CONFIG["ruby_version"]
+        else
+          File.join RbConfig::CONFIG["libdir"], 'ruby', 'gems', 
+            RbConfig::CONFIG["ruby_version"]
+        end
+      end
+
+      def activate(name, version = nil)
+        gem = if version.nil?
+          self.gems(name).last
+        else
+          # TODO: > = ~ version
+          # >= 0
+          version.delete!("<>=~ ")
+          self.gem(name, version)
+        end
+        self.loaded_gems.push(File.basename(File.dirname(gem)))
+        $:.push(gem)
+      end
+
+      def deactivate(name, version = nil)
+        if version.nil?
+          $:.each do |path|
+            if File.dirname(path).match(/^#{Regexp::quote(name)}-/)
+              $:.delete!(path)
+            end
+          end
+        else
+          # TODO: > = ~ version
+          # >= 0
+          version.delete!("<>=~ ")
+          libdir = self.gem(name, version)
+          $:.delete(libdir) if $:.include?(libdir)
+        end
       end
     end
   end
-end
 
-module Kernel
-  def gem(name, version = nil)
-    Microgem.activate(name, version)
-  end
+  module Kernel
+    def gem(name, version = nil)
+      Microgems.activate(name, version)
+    end
 
-  alias_method :__require__, :require
-  def require(file)
-    __require__(file)
-  rescue LoadError
-    gem file
-    __require__(file)
+    alias_method :__require__, :require
+    def require(file)
+      Microgems.activate(file) rescue nil
+      __require__(file)
+    rescue LoadError
+      path_info
+      raise $!
+    end
+
+    private
+    def path_info
+      if $DEBUG or $VERBOSE
+        STDERR.puts "Load paths: #{$:.inspect}"
+        STDERR.puts "Loaded gems: #{::Microgems.loaded_gems.inspect}"
+      end
+    end
   end
 end
